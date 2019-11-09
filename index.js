c9d95e96d7463f5fd9abf2a5d092271ce794fe88
sashayao <44731588+sashayao@users.noreply.github.com>
2019-11-09 13:21:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000
+var app = express();
+
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {res.render('pages/index')});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.get('/home', (req,res) => { res.render('pages/home')});
+
+app.get('/maps', (req,res) => { res.render('pages/maps')});
